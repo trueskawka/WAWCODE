ab8daa4db3adb9ec76fc78196829d1a6e050c1f3
Adam Zadrzny <Adam.Zadrozny@fuw.edu.pl>
2016-02-27 16:10:08 +0100

    Dane testowe

--- scraping/pyLoadToDataBase.py
+++ scraping/pyLoadToDataBase.py
@@ -1,14 +1,25 @@
 import sys
+import glob
+import random
+import codecs
 from xml.dom import minidom
 
 initial = """Toilet.destroy_all
-toi1 = Toilet.create(lat: '52.2169803', lon:'20.9952883', disabledfriendly: 0, momfriendly: 0, prize: 0  )
-toi2 = Toilet.create(lat: '52.2463708', lon:'21.0194412', disabledfriendly: 0, momfriendly: 0, prize: 0  )"""
-initialNum = 3
+"""
+initialNum = 1
 
-patternToi = "toi%d = Toilet.create(lat: '%s', lon:'%s', disabledfriendly: 0, momfriendly: 0, prize: 0  )"
+patternToi = "toi%d = Toilet.create(lat: '%s', lon:'%s', disabledfriendly: %d, momfriendly: %d, prize: '%d', name: '%s', adress: '%s', desc:'%s',type: '%s')"
 
-def extractData(fname, nn):
+types =[
+('toaleta','Toaleta'),
+('cafe','Kawiarnia'),
+('restauracja','Restauracja'),
+('mcdonalds','McDonalds'),
+('CH','Centrum Handlowe'),
+('PR','Parkuj i Jedz')
+]
+
+def extractData(fname, nn, opis):
 	#print fname
 	DOMTree = minidom.parse(fname) # AZ : wczytanie pliku
 	collection = DOMTree.documentElement
@@ -18,16 +29,24 @@ def extractData(fname, nn):
 		if p.hasAttribute("lat") and p.hasAttribute("lon"):
 			lat = p.getAttribute("lat")
 			lon = p.getAttribute("lon")
+			name = opis
+			addr = p.getAttribute("display_name")
+			desc = ''
+			ttype = p.getAttribute("type")
 			nn = nn + 1
-			dataInsert = patternToi % (nn, lat, lon)
+			dataInsert = patternToi % (nn, lat, lon, random.randint(0,1), random.randint(0,1), random.randint(0,2),name,'',desc,ttype)
 			print dataInsert
 	return nn
 
 
 if __name__ == "__main__":
 	print initial
-	fnamePattern = "data_%d.xml"
 	nn = initialNum
-	for i in range(9):
-		fname = fnamePattern % (i)
-		nn = extractData(fname, nn)
+
+	for tt in types:
+		(mm,opis) = tt
+		pattern = "%s_*.xml" % (mm)
+		flist = glob.glob(pattern)			
+		
+		for ff in flist:
+			nn = extractData(ff, nn, opis)
